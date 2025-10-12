89f7e76bb311656bdd62d2b65cdfe3dac1f57d94
Sergey <103505035+Stav26@users.noreply.github.com>
2025-10-12 12:23:49 +0300

    Оптимизирован код в функции get_last_message в части работы с методом messages.getConversations

--- telbotvk.py
+++ telbotvk.py
@@ -6,21 +6,28 @@ import sys
 from logger_config import setup_logger
 from read_write_json import *
 
-
-
 #Извлекаем из беседы в vk последнее сообщение и записываем last_message_id в файл
-def vkapi():
+def get_last_message():
     try:
         api = vk.API(access_token=token_vk, v = '5.199')
         full_list = api.messages.getConversations()
-        last_message_id = int(full_list['items'][0]['conversation']['last_conversation_message_id'])
-        if last_message_id != int(read_id):
-            data["vk"]["last_message_id"] = last_message_id
-            write_json_file(data)
-            return full_list['items'][0]['last_message']['text']
-        else:
-            logging.info(f"Новые сообщения отсутсвуют\n")
-            sys.exit(1)
+        for items in full_list['items']:
+            #Проверяем есть ли id чата указанного в settings в vk
+            if items['last_message']['peer_id'] == chat_id_vk:
+                last_message_id = items['last_message']['conversation_message_id']
+                #Если чат найден проверяем есть ли id последнего сообщения в settings
+                if last_message_id != read_id:
+                    #Если id последнего сообщения не совпадаеют то записываем его в settings
+                    data["vk"]["last_message_id"] = last_message_id
+                    write_json_file(data)
+                    #Возращаем текст последнего сообщения
+                    return items['last_message']['text']
+                else:
+                    logging.info(f"Новые сообщения отсутсвуют\n")
+                    sys.exit(1)
+            else:
+                logging.info(f"Чат в vk отсутсвует\n")
+                sys.exit(1)
     except vk.exceptions.VkAPIError as e:
         logging.error(f"{e}")
         return e
@@ -33,13 +40,13 @@ def vkapi():
 def send():
     try:
         logging.info(f"Отправляю содержание сообщения в бот")          
-        telebot.TeleBot(token_tg).send_message(chat_id, vkapi())
+        telebot.TeleBot(token_tg).send_message(chat_id_tg, vkapi())
         logging.info(f"Отправлено\n")
     except telebot.apihelper.ApiTelegramException as e:
         for attr in ['description']:
             if hasattr(e, attr):
                 if not "Bad Request: message text is empty" in getattr(e, attr):
-                    telebot.TeleBot(token_tg).send_message(chat_id, e)
+                    telebot.TeleBot(token_tg).send_message(chat_id_tg, e)
                     logging.error(f"{getattr(e, attr)}\n")
     except:
         pass
@@ -50,14 +57,15 @@ if __name__ == "__main__":
     
     if read_json_file() is not None:
        token_tg = read_json_file()["telegram"]["bot_token"]
-       chat_id = read_json_file()["telegram"]["chat_id"]
+       chat_id_tg = read_json_file()["telegram"]["chat_id"]
+       chat_id_vk = read_json_file()["vk"]["chat_id"]
        token_vk = read_json_file()["vk"]["access_token"]
        read_id = read_json_file()["vk"]["last_message_id"]
        logging.info(f"Запускаю скрипт {os.path.basename(__file__)}")
        logging.info(f"Извлечение из беседы в vk, id последнего сообщения и запись last_message_id в файл")
-       vkapi()
+       get_last_message()
        send()
     else:
         logging.error("Файл не найден, невозможно продолжить\n") 
-
         sys.exit(1)
+
